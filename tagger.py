5d4b0b558890081f73aea1f612a471a229496884
gianluca farinaccio <gianluca.farinaccio@gmail.com>
2024-07-08 21:54:13 +0200

    files

---
+++ tagger.py
@@ -0,0 +1,6 @@
+import cv2
+
+
+# 1. read json file 
+# 2. create an array of tuple - 9 tuple - one for turns
+# 3. calculate range of frames associated to secs of start and end - based on video's fps (25?)
