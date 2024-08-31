00738c92bbed0a97f9a4e8c345e27356883e290f
gianluca farinaccio <gianluca.farinaccio@gmail.com>
2024-08-31 14:34:55 +0200

    update: dataset split script now splits by turns and type of set

--- dataset-split.py
+++ dataset-split.py
@@ -3,17 +3,19 @@ import random
 import shutil
 
 # Define paths
-dataset_path = 'dataset'
+dataset_path = 'f1-monza-dataset'
 images_path = os.path.join(dataset_path, 'images')
-labels_path = os.path.join(dataset_path, 'labels')
+turns = ["0-prima-variante", "1-biassono", "2-seconda-variante","3-lesmo-uno", "4-lesmo-due", "5-ascari-uno", "6-ascari-due", "7-parabolica"]
+
 
 # Train-test split ratio
-train_ratio = 0.8
-val_ratio = 0.1
-test_ratio = 0.1
+train_ratio = 0.70
+val_ratio = 0.15
+test_ratio = 0.15
 
 # Get list of image files
 images = [f for f in os.listdir(images_path) if f.endswith('.jpg')]
+print(images)
 random.shuffle(images)
 
 # Calculate split indices
@@ -27,15 +29,15 @@ test_images = images[val_split:]
 
 # Move files
 def move_files(file_list, src_folder, dest_folder):
-    os.makedirs(dest_folder, exist_ok=True)
     for file in file_list:
-        shutil.move(os.path.join(src_folder, file), dest_folder)
+        subdir = turns[int(file.split("__")[1])]
+        print(subdir)
+        dest_folder_turns = os.path.join(dest_folder, subdir)
+        print(dest_folder_turns)
+        shutil.move(os.path.join(src_folder, file), dest_folder_turns)
 
 move_files(train_images, images_path, os.path.join(images_path, 'train'))
-move_files(train_images, labels_path, os.path.join(labels_path, 'train'))
 
 move_files(val_images, images_path, os.path.join(images_path, 'val'))
-move_files(val_images, labels_path, os.path.join(labels_path, 'val'))
 
 move_files(test_images, images_path, os.path.join(images_path, 'test'))
-move_files(test_images, labels_path, os.path.join(labels_path, 'test'))
